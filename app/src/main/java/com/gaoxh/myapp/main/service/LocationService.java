91f269ba799bbd5b7aad14e61e936a9173b18f6e
高雄辉 <gaoxionghui@sunmi.com>
2019-01-31 17:14:11 +0800

    修改注释

--- app/src/main/java/com/gaoxh/myapp/main/service/LocationService.java
+++ app/src/main/java/com/gaoxh/myapp/main/service/LocationService.java
@@ -62,63 +62,63 @@ public class LocationService extends Service {
         LocationClientOption option = new LocationClientOption();
 
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-//可选，设置定位模式，默认高精度
-//LocationMode.Hight_Accuracy：高精度；
-//LocationMode. Battery_Saving：低功耗；
-//LocationMode. Device_Sensors：仅使用设备；
+        //可选，设置定位模式，默认高精度
+        //LocationMode.Hight_Accuracy：高精度；
+        //LocationMode. Battery_Saving：低功耗；
+        //LocationMode. Device_Sensors：仅使用设备；
 
         option.setCoorType("bd09ll");
-//可选，设置返回经纬度坐标类型，默认GCJ02
-//GCJ02：国测局坐标；
-//BD09ll：百度经纬度坐标；
-//BD09：百度墨卡托坐标；
-//海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
+        //可选，设置返回经纬度坐标类型，默认GCJ02
+        //GCJ02：国测局坐标；
+        //BD09ll：百度经纬度坐标；
+        //BD09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
 
         option.setScanSpan(5000);
-//可选，设置发起定位请求的间隔，int类型，单位ms
-//如果设置为0，则代表单次定位，即仅定位一次，默认为0
-//如果设置非0，需设置1000ms以上才有效
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
 
         option.setOpenGps(true);
-//可选，设置是否使用gps，默认false
-//使用高精度和仅用设备两种定位模式的，参数必须设置为true
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
 
         option.setLocationNotify(true);
-//可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
 
         option.setIgnoreKillProcess(false);
-//可选，定位SDK内部是一个service，并放到了独立进程。
-//设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
 
         option.SetIgnoreCacheException(false);
-//可选，设置是否收集Crash信息，默认收集，即参数为false
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
 
         option.setWifiCacheTimeOut(5 * 60 * 1000);
-//可选，V7.2版本新增能力
-//如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
+        //可选，V7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
 
         option.setEnableSimulateGps(false);
-//可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
 
         option.setIsNeedAddress(true);
-//可选，是否需要地址信息，默认为不需要，即参数为false
-//如果开发者需要获得当前点的地址信息，此处必须为true
+        //可选，是否需要地址信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的地址信息，此处必须为true
 
         option.setIsNeedLocationDescribe(true);
-//可选，是否需要位置描述信息，默认为不需要，即参数为false
-//如果开发者需要获得当前点的位置信息，此处必须为true
+        //可选，是否需要位置描述信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的位置信息，此处必须为true
 
         mLocationClient.setLocOption(option);
-//mLocationClient为第二步初始化过的LocationClient对象
-//需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
-//更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
 
         new Thread() {
             @Override
             public void run() {
                 mLocationClient.start();
-//mLocationClient为第二步初始化过的LocationClient对象
-//调用LocationClient的start()方法，便可发起定位请求
+                //mLocationClient为第二步初始化过的LocationClient对象
+                //调用LocationClient的start()方法，便可发起定位请求
                 mLocationClient.startIndoorMode();// 开启室内定位模式（重复调用也没问题），开启后，定位SDK会融合各种定位信息（GPS,WI-FI，蓝牙，传感器等）连续平滑的输出定位结果；
             }
         }.start();
